1dcfcfe8ce5f9799a905ae5ec4e1a3ec0c3b332c
Norbert Szulc <norbert@not7cd.net>
2023-01-01 18:05:47 +0100

    Add config.h

---
+++ include/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+#define APPEND_MAC_TO_HOSTNAME
+
+const char* const hostname = "vortex-cam-1";
+const char* const friendlyName = "Vortex Cam 1 top";
+const char* const mqttHost = "mqtt.hack";
+const char* const fallbackMqttIp = "192.168.88.20";
